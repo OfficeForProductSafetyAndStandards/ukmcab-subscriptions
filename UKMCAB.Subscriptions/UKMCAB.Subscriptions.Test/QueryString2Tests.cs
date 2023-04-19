8d83548d2de96bf78ce423bfc12dcf51d613d94c
kristan66 <kristan@hotmail.co.uk>
2023-04-19 21:09:38 +0100

    upd

--- UKMCAB.Subscriptions/UKMCAB.Subscriptions.Test/QueryString2Tests.cs
+++ UKMCAB.Subscriptions/UKMCAB.Subscriptions.Test/QueryString2Tests.cs
@@ -9,7 +9,7 @@ public class QueryString2Tests
     {
         var q1 = QueryString2.Parse("a=b&a=c&d=e&f=g&g=h").ToString();
         var q2 = QueryString2.Parse("?a=b&a=c&d=e&f=g&g=h&").ToString();
-        var q3 = QueryString2.Parse("&d=e&f=g&a=b&a=c&g=h&&&").ToString();
+        var q3 = QueryString2.Parse("&d=e&f=g&a=b&a=c&g=h&&&&").ToString();
         var q4 = QueryString2.Parse("&g=h&a=c&d=e&f=g&a=b").ToString();
 
         Assert.IsTrue(new[] { q1, q2, q3, q4 }.All(x => x.Equals(q1)));
